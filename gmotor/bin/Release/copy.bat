4a8fa165edee10f2a0e60438eff0407e4e2a3a67
Roman Vančura <roman.vancura@email.cz>
2014-01-28 22:36:08 +0100

    Added binaries

---
+++ gmotor/bin/Release/copy.bat
@@ -0,0 +1,5 @@
+"C:\Program Files (x86)\Microsoft\ILMerge\ILMerge.exe" /out:.\..\..\..\..\turnaj\pbrain-gmotor2012.exe gmotor.exe Engine.dll  
+
+del .\..\..\..\..\turnaj\*.log
+del .\..\..\..\..\turnaj\*.pdb
+del .\..\..\..\..\turnaj\partie\*.* /q
